47bff6d080a8d59b7f6adbb72e2f399a08859103
RandallTem <83577883+RandallTem@users.noreply.github.com>
2021-10-30 21:01:48 +0300

    Add 6 Round Mora Attack

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         /** Запуск атаки */
-        Mora5RoundAttack.Attack(0x6666666666666667L, 50);
+        Mora6RoundAttack.Attack(0x6666666666666667L, 50);
     }
 }
